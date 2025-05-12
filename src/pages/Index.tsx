56355dc48c2c0e3e067a9bc286c9291a182c8d7f
Rajneesh <rajneesh.k@codebrewinnovations.com>
2025-05-12 16:00:59 +0530

    logo update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -73,7 +73,8 @@ const Index = () => {
               <Facebook size={20} />
             </a> */}
             <a href="https://x.com/aussivo" target="_blank" className="text-muted-foreground hover:text-primary transition-colors duration-300 hover:scale-110 transform" aria-label="Twitter">
-              <Twitter size={20} />
+              {/* <Twitter size={20} /> */}
+             <img src="/x-logo.png" alt="Twitter" className="w-4 h-4" />
             </a>
             <a href="https://www.instagram.com/aussivo.offical?igsh=aGkwc2JlczJsMWZ0" target="_blank" className="text-muted-foreground hover:text-primary transition-colors duration-300 hover:scale-110 transform" aria-label="Instagram">
               <Instagram size={20} />
